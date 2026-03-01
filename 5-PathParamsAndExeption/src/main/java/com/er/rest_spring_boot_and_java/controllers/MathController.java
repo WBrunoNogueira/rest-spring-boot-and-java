97891e7ca39d270368cd5854de5722c9bc8aadf1
wenderson <wendersonbruno10@gmail.com>
2026-03-01 10:30:06 -0300

    MathController finalizado

--- 5-PathParamsAndExeption/src/main/java/com/er/rest_spring_boot_and_java/controllers/MathController.java
+++ 5-PathParamsAndExeption/src/main/java/com/er/rest_spring_boot_and_java/controllers/MathController.java
@@ -13,22 +13,24 @@ public class MathController {
     
     @RequestMapping(value="/sum/{numberOne}/{numberTwo}")
     public Double sum(@PathVariable("numberOne") String numberOne,
-                      @PathVariable("numberTwo") String numberTwo) throws Exception {
-        if (!isNumeric(numberOne) || !isNumeric(numberTwo)) {
-            throw new UnsupportedMathOperationException("Please set a numeric value!");
-        }
+                      @PathVariable("numberTwo") String numberTwo)
+	throws Exception {
+		if (!isNumeric(numberOne) || !isNumeric(numberTwo))
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
         return covertToDouble(numberOne) + covertToDouble(numberTwo);
     }    
-    
-    public static Double covertToDouble(String strNumber) {
-		if (strNumber == null) return 0d; 
+
+    public static Double covertToDouble(String strNumber) throws IllegalArgumentException{
+
+		if (strNumber == null || strNumber.isEmpty())
+			throw new UnsupportedMathOperationException("Please set a numeric value!");
 		String number = strNumber.replaceAll(",", ".");// Moeda Americana x Brasileira
 		return Double.parseDouble(number);
 	}
 
 	public static boolean isNumeric(String strNumber) {
-		if (strNumber == null) return false; 
-		String number = strNumber.replaceAll(",", ".");
+		if (strNumber == null || strNumber.isEmpty()) return false;
+		String number = strNumber.replace(",", ".");
 			return number.matches("[-+]?[0-9]*\\.?[0-9]+");
 	}
 }
